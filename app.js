60846eb695138c545469c45d1ab33a109caa2b56
최재민 <admin@chicken-moo.com>
2023-05-03 12:25:49 +0900

    0.0.0.0 open

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ module.exports = async function (fastify, opts) {
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
